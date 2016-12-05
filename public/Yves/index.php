adb09f62ae6309f53789b5f250480181328a6b13
Fabian Dörk <fabian.doerk@de.clara.net>
2016-12-05 15:03:26 +0100

    makes c3 inclusion dependant on the environment
    We basically differentiate between 2 environment modes, one for
    `production` and one for `development`. Only the ladder needs
    code coverage testing. We do not want this in production.

--- public/Yves/index.php
+++ public/Yves/index.php
@@ -12,6 +12,9 @@ if (extension_loaded('newrelic')) {
 define('APPLICATION', 'YVES');
 defined('APPLICATION_ROOT_DIR') || define('APPLICATION_ROOT_DIR', realpath(__DIR__ . '/../..'));
 
+if (getenv('application_env') == 'development') 
+  include APPLICATION_ROOT_DIR . '/c3.php';
+
 require_once APPLICATION_ROOT_DIR . '/vendor/autoload.php';
 
 Environment::initialize();
