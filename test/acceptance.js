bce5a94b737e2888e91b359e3cedb9f0a7b96d41
Felipe Santos <felipe.santos@de.clara.net>
2017-06-20 11:52:22 +0100

    Add modifiable test entries.

--- test/acceptance.js
+++ test/acceptance.js
@@ -1,35 +1,34 @@
-const BASE_URL = 'http://localhost:2380/';
+var config = require('./config');
 
 casper.test.begin('Testing Spryker Demoshop web page', function (test) {
     // Open home page and search for item
-    casper.start(BASE_URL, function () {
-        test.assertTitle('Spryker Shop', 'Home Page title is Spryker Shop');
+    casper.start(config.baseUrl, function () {
+        test.assertTitle(config.title, 'Home Page title is ' + config.title);
         test.comment('Fill search field');
         this.fill('form[action="/search"]', {
-            q: 'dell chromebook 13'
+            q: config.item.search
         }, true);
     });
 
     // Click on requested item
     casper.then(function () {
-        test.assertExists('a[href="/en/dell-chromebook-13-63"]');
-        this.click('a[href="/en/dell-chromebook-13-63"]');
+        test.assertExists('a[href="' + config.item.href + '"]');
+        this.click('a[href="' + config.item.href + '"]');
     });
 
     // Add item to cart
     casper.then(function () {
-        test.assertTitle('DELL Chromebook 13', 'Title is DELL Chromebook 13');
+        test.assertTitle(config.item.title, 'Title is ' + config.item.title);
         test.comment('Add item to cart');
         test.assertSelectorHasText('h5', 'Product Variants');
-        this.fill('form[method="GET"]', {
-            'attribute[processor_frequency]': '2 GHz'
-        }, false);
+        this.fill('form[method="GET"]', config.item.variants, false);
     });
 
     casper.then(function () {
-        const btnDisabled = this.evaluate(function (btn) {
+        var btnDisabled = this.evaluate(function (btn) {
             return document.querySelector(btn).hasAttribute('disabled');
         }, 'button.button.expanded.success');
+        test.comment('Test if "Add to cart" button is enabled');
         test.assertFalsy(btnDisabled);
         test.assertExists('button.button.expanded.success');
         this.click('button.button.expanded.success');
@@ -40,7 +39,7 @@ casper.test.begin('Testing Spryker Demoshop web page', function (test) {
         test.assertSelectorHasText('h3', 'Cart');
         test.comment('Proceed to checkout');
         // this.click('a.button.expanded.success');     // Uncomment when links are working
-        casper.open(BASE_URL + 'checkout');     // This should be taken out when the links are working properly
+        casper.open(config.baseUrl + 'checkout');     // This should be taken out when the links are working properly
     });
 
     // Fill checkout-guest form
@@ -48,13 +47,7 @@ casper.test.begin('Testing Spryker Demoshop web page', function (test) {
         this.click('input#guest');
         test.assertSelectorHasText('.__checkout-proceed-as-method.__is-shown', ' Order as guest');
         test.comment('Fill user form');
-        this.fill('form.callout', {
-            'guestForm[customer][salutation]': 'Mr.',
-            'guestForm[customer][first_name]': 'John',
-            'guestForm[customer][last_name]': 'Doe',
-            'guestForm[customer][email]': 'john@doe.com',
-            'guestForm[customer][accept_terms]': true
-        }, true);
+        this.fill('form[name="guestForm"]', config.guest.register, true);
     });
 
     // Fill address information
@@ -62,16 +55,7 @@ casper.test.begin('Testing Spryker Demoshop web page', function (test) {
         test.assertSelectorHasText('h3', 'Address');
         test.assertSelectorHasText('h4', 'Shipping Address');
         test.comment('Fill address form');
-        this.fill('form[name=addressesForm]', {
-            'addressesForm[shippingAddress][salutation]': 'Mr.',
-            'addressesForm[shippingAddress][first_name]': 'John',
-            'addressesForm[shippingAddress][last_name]': 'Doe',
-            'addressesForm[shippingAddress][address1]': 'That Street',
-            'addressesForm[shippingAddress][address2]': '1',
-            'addressesForm[shippingAddress][zip_code]': '12345',
-            'addressesForm[shippingAddress][city]': 'City',
-            'addressesForm[billingSameAsShipping]': true
-        }, true);
+        this.fill('form[name="addressesForm"]', config.guest.address, true);
     });
 
     casper.run(function () {
