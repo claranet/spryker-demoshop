2d2aad2caa6eae82711137dbc0a84bf5d564b96b
Felipe Santos <felipefss@gmail.com>
2017-07-20 15:31:17 +0100

    Make env vars consistent in travis.
    Add `setup_tests` to docker's ignore list.

--- scripts/ci/run_tests.sh
+++ scripts/ci/run_tests.sh
@@ -3,11 +3,11 @@
 set -e -o pipefail
 set -x
 
-PROJECT=${project:-spryker}
+PROJECT=${PROJECT:-spryker}
 YVES_SVC=yves
 
 ./docker/run devel up -d
 
 echo "Waiting for stack to come up (it may take several minutes)..."
-docker wait ${PROJECT}devel_init_1 \
-&& docker exec -e PUBLIC_YVES_DOMAIN=$YVES_SVC ${PROJECT}devel_yves_1 /entrypoint.sh codeception
+docker wait ${PROJECT}devel_init_1
+docker exec -e PUBLIC_YVES_DOMAIN=$YVES_SVC ${PROJECT}devel_yves_1 /entrypoint.sh codeception
