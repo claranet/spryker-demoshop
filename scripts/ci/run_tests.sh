503483a0d1068863cde8ec1b1ebc018be6947bf0
Fabian Dörk <fabian.doerk@de.clara.net>
2017-08-01 23:27:48 +0200

    Make env to be used for testing explicit

--- scripts/ci/run_tests.sh
+++ scripts/ci/run_tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 set -e -o pipefail
-set -x
 
 PROJECT=${PROJECT:-spryker}
+ENV=${ENV:-prod}
 YVES_SVC=yves
 
 echo "Waiting for stack to come up (it may take several minutes) ..."
-docker wait ${PROJECT}prod_init_1
-docker exec -e PUBLIC_YVES_DOMAIN=$YVES_SVC ${PROJECT}devel_yves_1 /entrypoint.sh codeception
+docker wait ${PROJECT}${ENV}_init_1
+docker exec -e PUBLIC_YVES_DOMAIN=$YVES_SVC ${PROJECT}${ENV}_yves_1 /entrypoint.sh codeception
