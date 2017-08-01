6c89dd08a653cc90168f4c4e68aacdf9aeaea241
Fabian Dörk <fabian.doerk@de.clara.net>
2017-08-01 22:56:39 +0200

    Debug failing tests

--- scripts/ci/run_tests.sh
+++ scripts/ci/run_tests.sh
@@ -6,6 +6,6 @@ set -x
 PROJECT=${PROJECT:-spryker}
 YVES_SVC=yves
 
-echo "Waiting for stack to come up (it may take several minutes)..."
-docker wait ${PROJECT}devel_init_1
+echo "Waiting for stack to come up (it may take several minutes) ..."
+docker wait ${PROJECT}prod_init_1
 docker exec -e PUBLIC_YVES_DOMAIN=$YVES_SVC ${PROJECT}devel_yves_1 /entrypoint.sh codeception
