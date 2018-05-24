409d6928128aa4fdf026691d158dc13704026aca
Fabian Dörk <fabian.doerk@de.clara.net>
2018-05-25 00:02:20 +0200

    Fix travis CI with multiple images to be built/published

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -5,12 +5,23 @@ set -e -o pipefail
 
 echo "Authenticating to docker hub ..."
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
+
+echo "Pulling CI images ..."
 docker pull $image:$tagci
+docker pull $image:$tagci-jenkins
+
+echo "Tagging images ..."
 docker tag $image:$tagci $image:$tag
-echo "Pushing image to docker hub: $image:$tag"
+docker tag $image:$tagci-jenkins $image:$tag-jenkins
+
+echo "Pushing images to docker hub ..."
 docker push $image:$tag
+docker push $image:$tag-jenkins
+
 if [ "$TRAVIS_TAG" == "$LATEST" ]; then
+  echo "Applying :latest tag to images ..."
   docker tag $image:$tag $image:latest
-  echo "Pushing image to docker hub: $image:latest"
+  docker tag $image:$tag $image:latest-jenkins
   docker push $image:latest
+  docker push $image:latest-jenkins
 fi
