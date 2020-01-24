0d1a9b8e9cb01f54ceb49c5b3157a82be8042f9f
Hicham Boulahboub <hicham.boulahboub@de.clara.net>
2020-01-24 11:52:30 +0100

    Add tcp wait for Redis before saving INIT done to it
    It could happen that the Redis is not available before the
    initialization is done. So we ensure the REDIS to be there before we
    store the information of "INIT done" into it.

--- docker/start.d/init/999_init_done.sh
+++ docker/start.d/init/999_init_done.sh
@@ -2,4 +2,5 @@
 
 # Prevent concurrent execution of exports: Synchronize init, deploy and cron
 # via redis
+wait_for_tcp_service $STORAGE_REDIS_HOST $STORAGE_REDIS_PORT
 storage_redis_command SET init "done"
