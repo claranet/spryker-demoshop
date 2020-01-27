090dc0e9f69aff9f0fa5f1a7ed76fcc0cb8696e4
Hicham Boulahboub <hicham.boulahboub@de.clara.net>
2020-01-27 10:30:41 +0100

    Fix RabbitMQ, Elsaticsearch, Jenkins configuration
    Make them dynamically multi-store aware and adjust the configurations to
    meet the latest Spryker constant and configuration schema.

--- docker/config_local.php
+++ docker/config_local.php
@@ -9,13 +9,18 @@ use Spryker\Shared\Customer\CustomerConstants;
 use Spryker\Shared\Kernel\Store;
 use Spryker\Shared\ProductManagement\ProductManagementConstants;
 use Spryker\Shared\Propel\PropelConstants;
+use Pyz\Shared\Scheduler\SchedulerConfig;
+use Spryker\Shared\SchedulerJenkins\SchedulerJenkinsConfig;
+use Spryker\Shared\SchedulerJenkins\SchedulerJenkinsConstants;
 use Spryker\Shared\Search\SearchConstants;
+use Spryker\Shared\SearchElasticsearch\SearchElasticsearchConstants;
 use Spryker\Shared\Session\SessionConstants;
-use Spryker\Shared\Setup\SetupConstants;
-use Spryker\Shared\Storage\StorageConstants;
+use Spryker\Shared\SessionRedis\SessionRedisConstants;
+use Spryker\Shared\StorageRedis\StorageRedisConstants;
 use Spryker\Shared\Twig\TwigConstants;
 use Spryker\Shared\ZedRequest\ZedRequestConstants;
 use Spryker\Shared\Event\EventConstants;
+use Spryker\Shared\Http\HttpConstants;
 use Spryker\Shared\Log\LogConstants;
 use Spryker\Yves\Log\Plugin\YvesLoggerConfigPlugin;
 use Spryker\Zed\Log\Communication\Plugin\ZedLoggerConfigPlugin;
@@ -23,6 +28,13 @@ use Spryker\Shared\RabbitMq\RabbitMqEnv;
 
 use Monolog\Logger;
 
+# Get information about stores
+# FIXME: Use only one method either explude env var or get from stores.php
+# ---
+$stores_indexed = explode(' ', getenv('STORES'));
+$stores = require(APPLICATION_ROOT_DIR . '/config/Shared/stores.php');
+$allStores = array_keys($stores);
+# ---
 
 $applicationStore = strtoupper(Store::getInstance()->getStoreName());
 
@@ -33,15 +45,20 @@ $sslEnabled     = ($httpScheme === 'https');
 $zedApiDomain   = getenv('ZED_API_HOST');
 $trustedProxies = [ '127.0.0.0/24', '10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16' ];
 
+$config[ApplicationConstants::YVES_TRUSTED_HOSTS]
+    = $config[HttpConstants::YVES_TRUSTED_HOSTS]
+    = [
+        $config[ApplicationConstants::HOST_YVES],
+        'localhost',
+    ];
 
 /**
  *   E L A S T I C S E A R C H
 **/
-$config[SearchConstants::ELASTICA_PARAMETER__TRANSPORT] = getenv('ELASTICSEARCH_PROTOCOL');
-$config[SearchConstants::ELASTICA_PARAMETER__HOST] = getenv('ELASTICSEARCH_HOST');
-$config[SearchConstants::ELASTICA_PARAMETER__PORT] = getenv('ELASTICSEARCH_PORT');
-$config[SearchConstants::ELASTICA_PARAMETER__INDEX_NAME] = strtolower($applicationStore).'_search';
-
+$config[SearchConstants::ELASTICA_PARAMETER__TRANSPORT] = $config[SearchElasticsearchConstants::TRANSPORT] = getenv('ELASTICSEARCH_PROTOCOL');
+$config[SearchConstants::ELASTICA_PARAMETER__HOST] = $config[SearchElasticsearchConstants::HOST] = getenv('ELASTICSEARCH_HOST');
+$config[SearchConstants::ELASTICA_PARAMETER__PORT] = $config[SearchElasticsearchConstants::PORT] = getenv('ELASTICSEARCH_PORT');
+$config[SearchConstants::ELASTICA_PARAMETER__INDEX_NAME] =  strtolower($applicationStore).'_search';
 
 /*   R E D I S
 
@@ -52,68 +69,71 @@ Use REDIS_STORE_DB_FACTOR to avoid store counter collisions between different
 stores.
 */
 
-$stores_indexed       = explode(' ',getenv('STORES'));
 $redisDatabaseCounter = array_search($applicationStore, $stores_indexed) + 1; // +1 to avoid "invalid db-index" error
 
-$config[StorageConstants::STORAGE_PREDIS_CLIENT_CONFIGURATION] = [
-  'protocol' => 'tcp',
-  'host'     => getenv('STORAGE_REDIS_HOST'),
-  'port'     => getenv('STORAGE_REDIS_PORT'),
-  'password' => getenv('STORAGE_REDIS_PASSWORD'),
-  'database' => $redisDatabaseCounter,
-];
+$config[StorageRedisConstants::STORAGE_REDIS_DATABASE] = $redisDatabaseCounter;
+$config[StorageRedisConstants::STORAGE_REDIS_PROTOCOL] = 'redis';
+$config[StorageRedisConstants::STORAGE_REDIS_HOST] = getenv('STORAGE_REDIS_HOST');
+$config[StorageRedisConstants::STORAGE_REDIS_PORT] = getenv('STORAGE_REDIS_PORT');
+$config[StorageRedisConstants::STORAGE_REDIS_PASSWORD] = getenv('STORAGE_REDIS_PASSWORD');
 
-$config[SessionConstants::YVES_SESSION_REDIS_DATABASE] = $redisDatabaseCounter;
-$config[SessionConstants::YVES_SESSION_REDIS_PROTOCOL] = 'redis';
-$config[SessionConstants::YVES_SESSION_REDIS_HOST]     = getenv('YVES_SESSION_REDIS_HOST');
-$config[SessionConstants::YVES_SESSION_REDIS_PORT]     = getenv('YVES_SESSION_REDIS_PORT');
-$config[SessionConstants::YVES_SESSION_REDIS_PASSWORD] = getenv('YVES_SESSION_REDIS_PASSWORD');
+$config[SessionRedisConstants::YVES_SESSION_REDIS_DATABASE] = $redisDatabaseCounter;
+$config[SessionRedisConstants::YVES_SESSION_REDIS_PROTOCOL] = 'redis';
+$config[SessionRedisConstants::YVES_SESSION_REDIS_HOST]     = getenv('YVES_SESSION_REDIS_HOST');
+$config[SessionRedisConstants::YVES_SESSION_REDIS_PORT]     = getenv('YVES_SESSION_REDIS_PORT');
+$config[SessionRedisConstants::YVES_SESSION_REDIS_PASSWORD] = getenv('YVES_SESSION_REDIS_PASSWORD');
 $config[SessionConstants::YVES_SESSION_PERSISTENT_CONNECTION] = true;
 
-$config[SessionConstants::ZED_SESSION_REDIS_DATABASE]  = $redisDatabaseCounter;
-$config[SessionConstants::ZED_SESSION_REDIS_PROTOCOL]  = 'redis';
-$config[SessionConstants::ZED_SESSION_REDIS_HOST]      = getenv('ZED_SESSION_REDIS_HOST');
-$config[SessionConstants::ZED_SESSION_REDIS_PORT]      = getenv('ZED_SESSION_REDIS_PORT');
-$config[SessionConstants::ZED_SESSION_REDIS_PASSWORD]  = getenv('ZED_SESSION_REDIS_PASSWORD');
+$config[SessionRedisConstants::ZED_SESSION_REDIS_DATABASE]  = $redisDatabaseCounter;
+$config[SessionRedisConstants::ZED_SESSION_REDIS_PROTOCOL]  = 'redis';
+$config[SessionRedisConstants::ZED_SESSION_REDIS_HOST]      = getenv('ZED_SESSION_REDIS_HOST');
+$config[SessionRedisConstants::ZED_SESSION_REDIS_PORT]      = getenv('ZED_SESSION_REDIS_PORT');
+$config[SessionRedisConstants::ZED_SESSION_REDIS_PASSWORD]  = getenv('ZED_SESSION_REDIS_PASSWORD');
 $config[SessionConstants::ZED_SESSION_PERSISTENT_CONNECTION] = true;
 unset($stores_indexed, $redisDatabaseCounter);
 
 /**
  *   J E N K I N S
  */
-$config[SetupConstants::JENKINS_BASE_URL]  = getenv('JENKINS_URL');
-$config[SetupConstants::JENKINS_DIRECTORY] = '/tmp/jenkins/jobs';
-
-/**
- *   R A B B I T M Q
- */
-// $config[RabbitMqEnv::RABBITMQ_HOST] = getenv('RABBITMQ_HOST');
-// $config[RabbitMqEnv::RABBITMQ_PORT] = getenv('RABBITMQ_PORT');
-// $config[RabbitMqEnv::RABBITMQ_USERNAME] = getenv('RABBITMQ_USERNAME');
-// $config[RabbitMqEnv::RABBITMQ_PASSWORD] = getenv('RABBITMQ_PASSWORD');
-// $config[RabbitMqEnv::RABBITMQ_VIRTUAL_HOST] = getenv('RABBITMQ_VIRTUAL_HOST');
-
-$config[RabbitMqEnv::RABBITMQ_CONNECTIONS] = [
-    $applicationStore => [
-        RabbitMqEnv::RABBITMQ_CONNECTION_NAME => $applicationStore.'-connection',
-        RabbitMqEnv::RABBITMQ_HOST => getenv('RABBITMQ_HOST'),
-        RabbitMqEnv::RABBITMQ_PORT => getenv('RABBITMQ_PORT'),
-        RabbitMqEnv::RABBITMQ_PASSWORD => getenv('RABBITMQ_PASSWORD'),
-        RabbitMqEnv::RABBITMQ_USERNAME => getenv('RABBITMQ_USERNAME'),
-        RabbitMqEnv::RABBITMQ_VIRTUAL_HOST => getenv('RABBITMQ_VIRTUAL_HOST'),
-        RabbitMqEnv::RABBITMQ_STORE_NAMES => [$applicationStore],
-        RabbitMqEnv::RABBITMQ_DEFAULT_CONNECTION => true
+$config[SchedulerJenkinsConstants::JENKINS_CONFIGURATION] = [
+    SchedulerConfig::SCHEDULER_JENKINS => [
+        SchedulerJenkinsConfig::SCHEDULER_JENKINS_BASE_URL => getenv('JENKINS_URL'),
     ],
 ];
 
 /**
+ *   R A B B I T M Q
+ */
+$rabbbitConfig = [];
+$config[RabbitMqEnv::RABBITMQ_API_HOST] = getenv('RABBITMQ_HOST');
+$config[RabbitMqEnv::RABBITMQ_API_PORT] = getenv('RABBITMQ_PORT');
+$config[RabbitMqEnv::RABBITMQ_API_USERNAME] = getenv('RABBITMQ_USERNAME');
+$config[RabbitMqEnv::RABBITMQ_API_PASSWORD] = getenv('RABBITMQ_PASSWORD');
+$config[RabbitMqEnv::RABBITMQ_API_VIRTUAL_HOST] = getenv('RABBITMQ_VHOST');
+foreach($allStores as $store) {
+  $rabbbitConfig[$store] = [
+    RabbitMqEnv::RABBITMQ_CONNECTION_NAME => $store . '-connection',
+    RabbitMqEnv::RABBITMQ_HOST => getenv('RABBITMQ_HOST'),
+    RabbitMqEnv::RABBITMQ_PORT => getenv('RABBITMQ_PORT'),
+    RabbitMqEnv::RABBITMQ_PASSWORD => getenv('RABBITMQ_PASSWORD'),
+    RabbitMqEnv::RABBITMQ_USERNAME => $store . '_' . getenv('RABBITMQ_USERNAME'),
+    RabbitMqEnv::RABBITMQ_VIRTUAL_HOST => '/' . $store . '_' . getenv('RABBITMQ_VIRTUAL_HOST'),
+    RabbitMqEnv::RABBITMQ_STORE_NAMES => [$store],
+    RabbitMqEnv::RABBITMQ_DEFAULT_CONNECTION => (strcmp($store, $applicationStore) == 0 ? true : false)
+  ];
+  $config[RabbitMqEnv::RABBITMQ_CONNECTIONS][$store][RabbitMqEnv::RABBITMQ_DEFAULT_CONNECTION] = true;
+}
+$config[RabbitMqEnv::RABBITMQ_CONNECTIONS] = $rabbbitConfig;
+
+/**
  *   P O S T G R E S
  */
-$database2store = [
-  'DE' => getenv('ZED_DATABASE_DATABASE').'_de',
-  'AT' => getenv('ZED_DATABASE_DATABASE').'_de',
-  'US' => getenv('ZED_DATABASE_DATABASE').'_us',
-];
+
+$database2store = []; # Postgres Configuration
+foreach($allStores as $store) {
+  $database2store[$store] = getenv('ZED_DATABASE_DATABASE') . '_' . strtolower($store);
+}
+
 $config[PropelConstants::ZED_DB_ENGINE]   = $config[PropelConstants::ZED_DB_ENGINE_PGSQL];
 $config[PropelConstants::ZED_DB_USERNAME] = getenv('ZED_DATABASE_USERNAME');
 $config[PropelConstants::ZED_DB_PASSWORD] = getenv('ZED_DATABASE_PASSWORD');
